aa258ef441ed765a7e9fcc5598e570717554830c
hfpp2012copy <hfpp2012@gmail.com>
2024-06-12 11:28:52 +0800

    fix

--- app/sign-up/[[...sign-up]]/page.tsx
+++ app/sign-up/[[...sign-up]]/page.tsx
@@ -1,5 +1,16 @@
+import Footer from "@/components/ui/footer";
+import Header from "@/components/ui/header";
 import { SignUp } from "@clerk/nextjs";
 
 export default function Page() {
-  return <SignUp />;
+  return <>
+    <Header />
+    <main className="grow flex justify-center items-center h-screen">
+
+      <SignUp />
+
+    </main>
+
+    <Footer />
+  </>;
 }
