3e24a07530bb7379046a2f513b15c7f7eac83e3e
hfpp2012copy <hfpp2012@gmail.com>
2024-06-13 09:49:35 +0800

    fix: card

--- app/(default)/page.tsx
+++ app/(default)/page.tsx
@@ -4,27 +4,26 @@ export const metadata = {
 }
 
 import Hero from '@/components/hero'
-import PressLogos from '@/components/press-logos'
-import Features from '@/components/features'
-import Features02 from '@/components/features-02'
+// import PressLogos from '@/components/press-logos'
+// import Features from '@/components/features'
+// import Features02 from '@/components/features-02'
 import Pricing from '@/components/pricing'
 import Testimonials from '@/components/testimonials'
-import Resources from '@/components/resources'
-import Cta from '@/components/cta'
+// import Resources from '@/components/resources'
+// import Cta from '@/components/cta'
 import PlatFormCard from '@/components/platform-card'
 
 export default function Home() {
   return (
     <>
       <Hero />
-      <PlatFormCard />
-      <PressLogos />
+      {/* <PressLogos />
       <Features />
-      <Features02 />
+      <Features02 /> */}
       <Pricing />
       <Testimonials />
-      <Resources />
-      <Cta />
+      {/* <Resources />
+      <Cta /> */}
     </>
   )
 }
